c608155f189ec3196fb04664be60fd04cec695f9
JeHuiPark <pjh2359@gmail.com>
2020-07-11 19:19:08 +0900

    MockMvc 필터설정을 변경하라
    * UTF8 인코딩 필터 추가

--- boot2-2-x-mock-mvc-sample/src/test/java/boot2_2_x/mock/mvc/sample/SampleApplicationTest.java
+++ boot2-2-x-mock-mvc-sample/src/test/java/boot2_2_x/mock/mvc/sample/SampleApplicationTest.java
@@ -5,15 +5,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Map;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-@AutoConfigureMockMvc
+@SampleConfig
 @SpringBootTest
 class SampleApplicationTest {
 
@@ -24,7 +22,6 @@ class SampleApplicationTest {
   ObjectMapper objectMapper;
 
   @Test
-  @DisplayName("testname")
   void test2() throws Exception {
     var json = objectMapper.writeValueAsString(Map.of("k", "한글"));
 
