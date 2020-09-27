0d6a94da93ede2256cae85d78a266193ebeea97f
JeHuiPark <pjh2359@gmail.com>
2020-09-27 22:49:47 +0900

    batch-example2 프로젝트 추가

---
+++ spring-batch-example/batch/batch-example2/src/main/java/com/example/batch/BatchAutoConfiguration.java
@@ -0,0 +1,9 @@
+package com.example.batch;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan
+@Configuration
+class BatchAutoConfiguration {
+}
