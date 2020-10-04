77e1f404ded0b8208512846c2daf54d60bb2e4be
JeHuiPark <pjh2359@gmail.com>
2020-10-04 23:41:27 +0900

    Something Entity 설정을 변경

--- spring-batch-example/mock-impl/src/main/java/com/example/impl/SomethingStatusEntity.java
+++ spring-batch-example/mock-impl/src/main/java/com/example/impl/SomethingStatusEntity.java
@@ -6,7 +6,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.Data;
+import org.hibernate.annotations.DynamicUpdate;
 
+@DynamicUpdate
 @Data
 @Table(name = "something")
 @Entity
@@ -16,7 +18,15 @@ class SomethingStatusEntity {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
 
-  private String status1 = "default";
+  private String status1;
 
-  private String status2 = "default";
+  private String status2;
+
+  void setStatus2(String status2) {
+    this.status2 = status2;
+  }
+
+  void setStatus1(String status1) {
+    this.status1 = status1;
+  }
 }
