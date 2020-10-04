c666b9e145d185341801d4fd92b5543bcc61575d
JeHuiPark <pjh2359@gmail.com>
2020-10-04 23:43:27 +0900

    batch 테스용 MongoDB 데이터 변경

--- spring-batch-example/mongo_init.js
+++ spring-batch-example/mongo_init.js
@@ -13,11 +13,38 @@ db.createUser(
 );
 
 db.createCollection('schedule');
-db.schedule.createIndex({'method': 1});
+
 var documents = [];
 for (var i = 1; i <= 10000; i++) {
-  documents.push({_id:i, method: ['function1', 'function2']});
+  documents.push({
+    target: NumberLong(i),
+    status: 'reservation',
+    method: 'function1',
+    begin: ISODate("2020-01-01T00:00:00.000Z"),
+    end: ISODate("2021-01-01T00:00:00.000Z"),
+    audit: 'user'
+  });
+  documents.push({
+    target: NumberLong(i),
+    status: 'reservation',
+    method: 'function2',
+    begin: ISODate("2020-01-01T00:00:00.000Z"),
+    end: ISODate("2021-01-01T00:00:00.000Z"),
+    audit: 'user'
+  });
 }
+
 db.schedule.insertMany(documents, {
   ordered: false
+});
+
+db.schedule.createIndex({
+  'target': 1
+});
+
+db.schedule.createIndex({
+  'begin': 1,
+  'end': 1,
+  'method': 1,
+  'status': 1
 });
